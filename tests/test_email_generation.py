ed7cacb9046bfdf5ab3b6c7e406118cc1abab782
Javier Riveros <javiiriveros@hotmail.com>
2020-10-14 16:15:26 -0500

    test newsletters embed and html pages

--- tests/test_email_generation.py
+++ tests/test_email_generation.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase, Client
+from django.test import TestCase, Client, tag
 from django.urls import reverse
 from django.urls import reverse
 from datetime import datetime, timedelta
@@ -7,18 +7,8 @@ from datetime import datetime, timedelta
 from djangoplicity.newsletters.models import NewsletterType, Newsletter, MailerParameter, Mailer
 
 
-def create_mailer_parameter(mailer=None, name='', value='', type='str', help_text=None):
-    return MailerParameter.objects.create(mailer=mailer, name=name, value=value, type=type, help_text=help_text)
-
-
-def create_mailer(plugin, name):
-    return Mailer.objects.create(plugin=plugin, name=name)
-
-
 class TestEmailGeneration(TestCase):
-    # LIST_NAME = 'esoepo-monitoring'
-    # LIST_PASSWORD = 'ohsiechu'
-    # LIST_BASEURL = 'http://www.eso.org/lists'
+    fixtures = ['newsletters']
 
     def setUp(self):
         self.client = Client()
@@ -31,96 +21,38 @@ class TestEmailGeneration(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        Mailer.objects.all().delete()
-        MailerParameter.objects.all().delete()
-        Newsletter.objects.all().delete()
-        NewsletterType.objects.all().delete()
-        cls.mailer = create_mailer(
-            plugin='djangoplicity.newsletters.mailers.MailChimpMailerPlugin',
-            name='Newsletters news list'
-        )
-        # cls.mailer_parameters = [
-        #     create_mailer_parameter(
-        #         mailer=cls.mailer,
-        #         name='enable_browser_link',
-        #         value=True,
-        #         type='bool',
-        #         help_text="Enable 'view in browser' link"
-        #     ),
-        #     create_mailer_parameter(
-        #         mailer=cls.mailer,
-        #         name='list_id',
-        #         value='ed25775a52',
-        #         type='str',
-        #         help_text='MailChimp list id - must be defined in djangoplicity.'
-        #     )
-        # ]
-        cls.newsletter_type = NewsletterType.objects.create(
-            name="ESA/Hubble News",
-            slug="hubblenews",
-            default_from_name="ESA/Hubble Information Centre",
-            default_from_email="hubble@eso.org",
-            subject_template="ESA/Hubble",
-            text_template="*******************************************",
-            html_template="""
-            {% extends "newsletters/base.html" %}
-            {% block header %}
-                {% with width="700" height="280" color="#000000" logo_width="140" logo_url=base_url|add:MEDIA_URL|add:"esa_nl_logo.png" %}
-                {% block header_content %}
-                {% if data.release %}
-                    {% with text_color="#ffffff" header_title="ESA/Hubble News" header_subtitle=data.release.release_date|date %}
-                    {% with release_date=data.release.release_date link_url=base_url|add:data.release.get_absolute_url pretitle="ESA/Hubble "|add:data.release.release_type.name|add:" "|add:data.release.id embargo="" title=data.release.title subtitle=data.release.subtitle main_image=data.release.main_image %}
-                    {% include "newsletters/includes/header.html" %}
-                    {% endwith %}
-                    {% endwith %}
-                {% else %}
-                    {% with text_color="#ffffff" header_title="ESA/Hubble News" header_subtitle="" %}
-                    {% include "newsletters/includes/header.html" %}
-                    {% endwith %}
-                {% endif %}
-            {% endblock %}
-                {% endwith %}
-            {% endblock %}""",
-            archive=True,
-            local_archive=False,
-            internal_archive=False,
-            sharing=False,
-            subscribe_text="""<p style="text-align:right; padding-right: 20px">
-                <a href="https://www.spacetelescope.org/subscribe/">Click here</a> to subscribe.
-            </p>""",
-        )
-        cls.newsletter_type.mailers.add(cls.mailer)
-        cls.newsletter = Newsletter.objects.create(
-            type=cls.newsletter_type, frozen=False,
-            start_date=datetime.now(),
-            end_date=datetime.now() + timedelta(minutes=5),
-            from_name='ESA/Hubble Information Centre',
-            from_email='hubble@eso.org',
-            subject='ESA/Hubble Science Release',
-            text='',
-            html='',
-            editorial_subject='',
-            editorial="""<p>Editorial</p>""",
-            editorial_text="""
-            For Your Information:
-            ESA/Hubble Science Release 
-            ---
-            European Southern Observatory
-            Email: hubble@eso.org"""
-        )
+        cls.mailer = Newsletter.objects.get(pk=10000)
+        cls.newsletter_type = NewsletterType.objects.get(pk=10000)
+        cls.newsletter = Newsletter.objects.get(pk=10000)
+
+    def test_newsletter_admin_html(self):
+        """Test newsletter html on admin is properly rendered"""
+        url = '/admin/newsletters/newsletter/%s/html/' % self.newsletter.pk
+        response = self.client.get(url, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.newsletter.editorial)
+
+    def test_newsletter_admin_text(self):
+        """Test newsletter text on admin is properly rendered"""
+        url = '/admin/newsletters/newsletter/%s/text/' % self.newsletter.pk
+        response = self.client.get(url, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.newsletter.editorial_text)
 
     def test_newsletter_detail_html(self):
         """Test newsletter html is properly rendered"""
-        url = '/newsletters/%s/html/%s' % (self.newsletter_type.slug, self.newsletter.pk)
-        res = self.client.get(url, follow=True)
+        url = '/newsletters/%s/html/%s/' % (self.newsletter_type.slug, self.newsletter.pk)
+        response = self.client.get(url, follow=True)
 
-        self.assertEqual(res.status_code, 200)
-        self.assertContains(res, self.newsletter.editorial_subject)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, """<iframe src="/newsletters/hubble-news/htmlembed/{0}/" width="100%" scrolling="no" frameBorder="0" onload='javascript:resizeIframe(this);'></iframe>""".format(self.newsletter.pk))
 
     def test_newsletter_embed_html(self):
         """Test newsletter embed html is properly rendered"""
-        url = '/newsletters/%s/htmlembed/%s' % (self.newsletter_type.slug, self.newsletter.pk)
-        res = self.client.get(url, follow=True)
+        url = '/newsletters/%s/htmlembed/%s/' % (self.newsletter_type.slug, self.newsletter.pk)
+        response = self.client.get(url, follow=True)
 
-        self.assertEqual(res.status_code, 200)
-        self.assertContains(res, self.newsletter.editorial_subject)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.newsletter.editorial)
